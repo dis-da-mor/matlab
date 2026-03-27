3bc356befd183baa0631218306513af42952432e
Matt-DESTROYER <matthewsalgorithm@gmail.com>
2026-03-27 11:11:00 +1100

    Fixed coloured errors smh

--- matlab/src/colours.rs
+++ matlab/src/colours.rs
@@ -1,7 +1,7 @@
 #[allow(non_snake_case)]
 pub mod Colours {
-	pub const RESET: &str = r"\e[0m";
-	pub const RED: &str = r"\e[31m";
+	pub const RESET: &str = "\x1b[0m";
+	pub const RED: &str = "\x1b[31m";
 }
 
 pub fn println_error(str: String) {
